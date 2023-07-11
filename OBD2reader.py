8e3abaa7ed219c33a09a29f4653f3a78e0688969
Rey Maci <rey@Reys-MacBook-Air.local>
2023-07-11 22:33:14 +0200

    Change it so it runs async

--- OBD2reader.py
+++ OBD2reader.py
@@ -1,4 +1,4 @@
-import time
+import asyncio
 import obd
 
 # Establish connection with the OBD-II adapter
@@ -6,28 +6,52 @@ connection = obd.OBD()
 
 # Define the list of PIDs (Parameter IDs) to monitor
 pids = [
-    obd.commands.RPM,  # Engine RPM
     obd.commands.SPEED,  # Vehicle speed
+    obd.commands.RPM,  # Engine RPM
+    obd.commands.THROTTLE_POS,  # Throttle position
     obd.commands.COOLANT_TEMP,  # Coolant temperature
     obd.commands.INTAKE_TEMP,  # Intake air temperature
-    obd.commands.MAF,  # Mass air flow rate
-    obd.commands.FUEL_LEVEL,  # Fuel level
-    obd.commands.FUEL_RATE,  # Fuel consumption rate
-    # Add more PIDs as needed from the previous list
+    # Add more PIDs as needed
 ]
 
+# Function to continuously read and print the non-speed and non-RPM parameters
+async def read_non_speed_rpm_params():
+    while True:
+        for pid in pids[2:]:  # Exclude speed and RPM from this loop
+            response = connection.query(pid)
+            if response.is_successful():
+                value = response.value
+                print(f"{pid.name}: {value}")
+            else:
+                print(f"Error retrieving {pid.name} data")
+        
+        print("---------------------")
+        await asyncio.sleep(1)
+
+# Function to continuously read and print the speed and RPM parameters
+async def read_speed_rpm_params():
+    while True:
+        for pid in pids[:2]:  # Only speed and RPM in this loop
+            response = connection.query(pid)
+            if response.is_successful():
+                value = response.value
+                print(f"{pid.name}: {value}")
+            else:
+                print(f"Error retrieving {pid.name} data")
+        
+        print("---------------------")
+        await asyncio.sleep(0.1)
+
 # Main program loop
-while True:
-    # Iterate over each PID in the list and read its value
-    for pid in pids:
-        response = connection.query(pid)
-        if response.is_successful():
-            value = response.value
-            print(f"{pid.name}: {value}")
-        else:
-            print(f"Error retrieving {pid.name} data")
+async def main():
+    # Create tasks for reading non-speed and non-RPM parameters, and speed and RPM parameters
+    tasks = [
+        asyncio.create_task(read_non_speed_rpm_params()),
+        asyncio.create_task(read_speed_rpm_params())
+    ]
 
-    print("---------------------")
+    # Run the tasks concurrently
+    await asyncio.gather(*tasks)
 
-    # Wait for 1 second before reading the values again
-    time.sleep(1)
+# Run the main program loop
+asyncio.run(main())

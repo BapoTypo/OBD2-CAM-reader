ebc7cad75f768c7e73d5ee41d280e881a9024519
BapoHEHE <alendm1@educbe.ca>
2023-07-17 15:57:01 -0600

    added button to start program

---
+++ Setup.bat
@@ -0,0 +1,14 @@
+@echo off
+
+REM Change the current directory to the specified location
+cd /d "C:\Program Files (x86)\Minimal ADB and Fastboot"
+
+REM Execute the "adb devices" command
+adb devices
+
+REM Execute the "adb forward" command
+adb forward tcp:5277 tcp:5277
+
+REM Executes Android auto
+cd "C:/Users/Taken/AppData/Local/Android/Sdk/extras/google/auto"
+desktop-head-unit.exe
